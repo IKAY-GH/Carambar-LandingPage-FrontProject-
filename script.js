259e438136ec1ca72e04b3a17338a577d9321987
CAYUELA Annick <annickcayuela.ac@gmail.com>
2025-06-24 00:46:06 +0200

    styleV4

--- script.js
+++ script.js
@@ -1,16 +1,26 @@
-const btn = document.getElementById('btn-blague');
-const questionEl = document.getElementById('question');
-const answerEl = document.getElementById('answer');
-
-btn.addEventListener('click', async () => {
+btnBlague.addEventListener('click', async () => {
   try {
     const res = await fetch('https://carambar-landingpage-backproject.onrender.com/api/v1/blagues/random');
     const data = await res.json();
+
     questionEl.textContent = data.question;
     answerEl.textContent = data.answer;
+
+    // Cacher la réponse
+    answerEl.classList.add('hidden');
+    // Afficher le bouton réponse
+    btnReponse.classList.remove('hidden');
   } catch (err) {
     questionEl.textContent = "Erreur lors du chargement";
     answerEl.textContent = "";
+    btnReponse.classList.add('hidden');
     console.error(err);
   }
+});
+
+btnReponse.addEventListener('click', () => {
+  // Afficher la réponse
+  answerEl.classList.remove('hidden');
+  // Cacher le bouton réponse
+  btnReponse.classList.add('hidden');
 });
